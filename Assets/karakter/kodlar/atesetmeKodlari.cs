765782a2b86a39577f17c7de2566c102b14f948c
metehan-41 <mthnyksk@gmail.com>
2025-10-17 20:00:55 +0300

    şarjor kapasitesi ve şarjor değiştirme animasyonu eklendi

--- Assets/karakter/kodlar/atesetmeKodlari.cs
+++ Assets/karakter/kodlar/atesetmeKodlari.cs
@@ -7,6 +7,10 @@ public class atesetmeKodlari : MonoBehaviour
     karakterKodlari canKontrol;
     Animator animator;
     public ParticleSystem muzzleAtes;
+
+    private float sarjor = 5;
+    private float cephane = 10;
+    private float sarjorKapasitesi = 5;
     void Start()
     {
         kamera = Camera.main;
@@ -20,7 +24,20 @@ public class atesetmeKodlari : MonoBehaviour
         {
             if (Input.GetMouseButton(0)) // sol click basildigi zaman
             {
-                animator.SetBool("atesEt", true);
+                if(sarjor > 0)
+                {
+                    animator.SetBool("atesEt", true);
+                }
+                if (sarjor <= 0)
+                {
+                    animator.SetBool("atesEt", false);
+                }
+                if(sarjor <= 0 && cephane > 0)
+                {
+                    animator.SetBool("sarjorDegistirme", true);
+                    
+                }
+                
             }
             else if (Input.GetMouseButtonUp(0))
             {
@@ -28,15 +45,27 @@ public class atesetmeKodlari : MonoBehaviour
             }
         }
     }
+    public void SarjorDegistirme()
+    {
+        cephane -= sarjorKapasitesi - sarjor;
+        sarjor = sarjorKapasitesi;
+        animator.SetBool("sarjorDegistirme", false);
+    }
 
     public void AtesEtme()
     {
-        muzzleAtes.Play();
-        Ray ray = kamera.ViewportPointToRay(new Vector3(0.5f, 0.5f, 0));//ekranin orta degerleri
-        RaycastHit hit;
-        if (Physics.Raycast(ray, out hit, Mathf.Infinity, npcLayer))
+        if (sarjor > 0)
         {
-            hit.collider.gameObject.GetComponent<npc1Kod>().HasarAl(); //npckodlarindaki hasarAl function ina gidiyor layer da npc i layer i vermek lazim ama
+            
+            muzzleAtes.Play();
+            Ray ray = kamera.ViewportPointToRay(new Vector3(0.5f, 0.5f, 0));//ekranin orta degerleri
+            RaycastHit hit;
+            if (Physics.Raycast(ray, out hit, Mathf.Infinity, npcLayer))
+            {
+                hit.collider.gameObject.GetComponent<npc1Kod>().HasarAl(); //npckodlarindaki hasarAl function ina gidiyor layer da npc i layer i vermek lazim ama
+            }
+            sarjor--;
         }
+       
     }
 }
